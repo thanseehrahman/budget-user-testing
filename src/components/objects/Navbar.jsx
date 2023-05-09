424a687c1205d1a49b0c593a069220ba49ae347d
Thanseeh Rahman <thanseehrahman004@gmail.com>
2023-05-09 21:08:16 +0530

    Warning Fixes

--- src/components/objects/Navbar.jsx
+++ src/components/objects/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { activateTransactionForm } from "../../redux/features/transactions/transactionSlice";
